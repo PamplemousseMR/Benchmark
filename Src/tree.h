b9420383177c2a850f28db086b7e8bbada0bf538
Maxime Lataix <maxime.lataix@etudiant.univ-reims.fr>
2017-02-21 17:13:28 +0100

    Ajout des commentaires de la classe tree

--- Src/tree.h
+++ Src/tree.h
@@ -1,9 +1,16 @@
 #ifndef TREE_H
 #define TREE_H
-
+/*!
+ * \file tree.h
+ * \brief contient la classe representant un arbre
+ * \author Appert Kevin, Bocahu Florent, Hun Tony, Lataix Maxime, Manciaux Romain, Peccard Remi
+ */
 #include <iostream>
 #include "node.h"
-
+/*!
+ * \class Tree
+ * \brief classe representant un arbre
+ */
 class Tree
 {
     private :
@@ -11,9 +18,17 @@ class Tree
         Node _root;
 
     public :
-
+        /*!
+         * \brief Tree::Tree Constructeur
+         */
         Tree();
+        /*!
+         * \brief Tree::Tree Constructeur par copie
+         */
         Tree(const Tree&);
+        /*!
+         * \brief Tree::Tree Destructeur
+         */
         ~Tree();
 };
 
