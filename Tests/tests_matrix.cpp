fdba8c216a80089dac39657938cf63d57d6df66d
pamplemousse26 <romain.manciaux@etudiant.univ-reims.fr>
2017-02-23 18:43:18 +0100

    ajout de test de matrix

---
+++ Tests/tests_matrix.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../Src/matrix.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------" << endl;
+    cout << "--test matrix--" << endl;
+    cout << "---------------" << endl;
+}
