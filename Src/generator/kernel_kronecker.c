e609b96dab299be9eb614cbda1237b9bb0695b50
pamplemousse26 <romain.manciaux@etudiant.univ-reims.fr>
2017-04-02 18:27:20 +0200

    debut du multi buffer

--- Src/generator/kernel_kronecker.c
+++ Src/generator/kernel_kronecker.c
@@ -1,5 +1,6 @@
 #include "kernel_kronecker.h"
 
+#ifdef _WIN32
 char* create_kernel_generator(unsigned int nbBuffers) {
 
     char numberTmp[10];
@@ -94,3 +95,98 @@ char* create_kernel_generator(unsigned int nbBuffers) {
     return function;
 
 }
+#else
+char* create_kernel_generator(unsigned int nbBuffers) {
+
+    char numberTmp[10];
+    unsigned int i;
+    char* function = (char*)xmalloc(sizeof(char) * FUNCTION_MAX_SIZE);
+
+    /* Construction du prototype */
+    strcpy(function, "__kernel void "KERNEL_KRONECKER_NAME"(const double A, const double B, const double C, __global mrg_state* seeds,const int scale, const unsigned long int edges_count_total");
+    for (i = 0; i < nbBuffers; ++i)
+    {
+        sprintf(numberTmp, "%d", i);
+        strcat(function, ", __global packed_edge* edges");
+        strcat(function, numberTmp);
+        strcat(function, " ,const unsigned long int edges_count");
+        strcat(function, numberTmp);
+    }
+    strcat(function, ")\n{\n");
+
+    strcat(function,    "\tunsigned int i;\n"\
+                        "\tunsigned int indice;\n"\
+                        "\tunsigned int edge;\n"\
+                        "\tpacked_edge* currentEdges;\n"\
+                        "\tsize_t id = get_global_id(0);\n"\
+                        "\tsize_t size = get_global_size(0);\n"\
+                        "\tint mul;\n"\
+                        "\tint ii_bit;\n"\
+                        "\tdouble ab = A+B;\n"\
+                        "\tdouble c_norm = C/(1-ab);\n"\
+                        "\tdouble a_norm = A/ab;\n"\
+                        "\tunsigned long int bornMin = (edges_count_total * id)/size;\n"\
+                        "\tunsigned long int bornMax = (edges_count_total * (id + 1))/size;\n\n"\
+                        "\tfor(int i=bornMin ; i<bornMax ; ++i)\n"\
+                        "\t{\n"\
+                        "\t\tindice = i;\n"\
+                        "\t\tcurrentEdges = edges0;\n");
+
+    for (i = 0; i < nbBuffers - 1; ++i)
+    {
+        strcat(function, "\t\tif (indice >= edges_count");
+        sprintf(numberTmp, "%d", i);
+        strcat(function, numberTmp);
+        strcat(function, ") {\n");
+        strcat(function, "\t\t\tindice -= edges_count");
+        strcat(function, numberTmp);
+        strcat(function, ";\n");
+        strcat(function, "\t\t\tcurrentEdges = edges");
+        sprintf(numberTmp, "%d", i + 1);
+        strcat(function, numberTmp);
+        strcat(function, ";\n");
+    }
+    strcat(function, "\t\t");
+    for (i = 0; i < nbBuffers - 1; ++i)
+        strcat(function, "}");
+
+    strcat(function,    "\t\tcurrentEdges[indice].v0 = 1;\n"\
+                        "\t\tcurrentEdges[indice].v1 = 1;\n"\
+                        "\t}\n\n"\
+                        "\tfor(i=0 ; i<scale ; ++i)\n"\
+                        "\t{\n"\
+                        "\t\tmul = 1<<i;\n"\
+                        "\t\tfor(edge=bornMin ; edge<bornMax ; ++edge)\n"\
+                        "\t\t{\n"\
+                        "\t\t\tii_bit = mrg_get_double_orig(&seeds[id])>ab;\n"\
+                        "\t\t\tindice = edge;\n\n"\
+                        "\t\t\tcurrentEdges = edges0;\n");
+
+    for (i = 0; i < nbBuffers - 1; ++i)
+    {
+        strcat(function, "\t\t\tif (indice >= edges_count");
+        sprintf(numberTmp, "%d", i);
+        strcat(function, numberTmp);
+        strcat(function, ") {\n");
+        strcat(function, "\t\t\t\tindice -= edges_count");
+        strcat(function, numberTmp);
+        strcat(function, ";\n");
+        strcat(function, "\t\t\t\tcurrentEdges = edges");
+        sprintf(numberTmp, "%d", i + 1);
+        strcat(function, numberTmp);
+        strcat(function, ";\n");
+    }
+    strcat(function, "\t\t\t");
+    for (i = 0; i < nbBuffers - 1; ++i)
+        strcat(function, "}");
+
+    strcat(function,    "\n\n\t\t\tcurrentEdges[indice].v1 +=  mul * ( mrg_get_double_orig(&seeds[id]) > (c_norm*ii_bit + a_norm*(!ii_bit)) );\n"\
+                        "\t\t\tcurrentEdges[indice].v0 +=  mul * ii_bit;\n"\
+                        "\t\t}\n"\
+                        "\t}\n"\
+                        "}\n");
+
+    return function;
+
+}
+#endif
